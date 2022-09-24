f39c5a66599929319398be6818285a9b7b894b97
maroqijalil <jalilpro31@gmail.com>
2022-09-24 11:20:35 +0700

    feat: init project and add funcs

---
+++ assignment-1/biodata.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/brianvoe/gofakeit/v6"
+)
+
+type Trainee struct {
+	id      int
+	name    string
+	address string
+	job     string
+	reason  string
+}
+
+func (trainee Trainee) print() {
+	fmt.Println("Nama:", trainee.name)
+	fmt.Println("Alamat:", trainee.address)
+	fmt.Println("Pekerjaan:", trainee.job)
+	fmt.Println("Alasan:", trainee.reason)
+}
+
+func main() {
+	arg := os.Args[1]
+	number, _ := strconv.Atoi(arg)
+
+	trainees := []Trainee{}
+
+	for i := 0; i < 50; i++ {
+		job := gofakeit.JobTitle()
+
+		trainees = append(trainees, Trainee{
+			name:    gofakeit.Name(),
+			address: gofakeit.Address().Address,
+			job:     job,
+			reason:  fmt.Sprintf("Saya ingin memaksimalkan posisi saya sebagai %s di %s.", job, gofakeit.Company()),
+		})
+	}
+
+	if (number <= 50) && (number >= 1) {
+		trainees[number-1].print()
+	} else {
+		fmt.Println("Peserta pelatihan dengan no.", number, "tidak ditemukan!")
+	}
+}
---
+++ assignment-1/go.mod
@@ -0,0 +1,5 @@
+module assignment-1
+
+go 1.19
+
+require github.com/brianvoe/gofakeit/v6 v6.19.0 // indirect
