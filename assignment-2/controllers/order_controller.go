304baa7b44cb54688ee59938c90b54a0b43ab988
maroqijalil <jalilpro31@gmail.com>
2022-10-01 22:15:18 +0700

    feat: init assignment-2

---
+++ assignment-2/controllers/order_controller.go
@@ -0,0 +1,19 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func CreateOrder(c *gin.Context) {
+
+}
+
+func GetOrders(c *gin.Context) {
+
+}
+
+func UpdateOrder(c *gin.Context) {
+
+}
+
+func DeleteOrder(c *gin.Context) {
+
+}
