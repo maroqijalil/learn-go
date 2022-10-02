0a02e4633139c17a85e353171153642e2d0cd66f
maroqijalil <jalilpro31@gmail.com>
2022-10-02 12:33:02 +0700

    feat: final change

--- assignment-2/controllers/order_controller.go
+++ assignment-2/controllers/order_controller.go
@@ -18,19 +18,22 @@ func (controller *Controllers) CreateOrder(context *gin.Context) {
 	}
 
 	database := controller.Database
-	if err := database.Omit("Items").Create(&order).Error; err != nil {
+	if err := database.
+		Select("Items").
+		Create(&order).Error; err != nil {
 		log.Println("failed to insert order into database")
 		context.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
 
-	items := order.Items
-	order.Items = nil
-	if err := database.Model(&order).Association("Items").Append(items); err != nil {
-		log.Println("failed to insert items into database")
-		context.AbortWithError(http.StatusInternalServerError, err)
-		return
-	}
+	//if err := database.
+	//	Model(&models.Order{OrderId: order.OrderId}).
+	//	Association("Items").
+	//	Append(order.Items); err != nil {
+	//	log.Println("failed to insert items into database")
+	//	context.AbortWithError(http.StatusInternalServerError, err)
+	//	return
+	//}
 
 	log.Println("succeeded to insert into database")
 	context.JSON(http.StatusCreated, gin.H{
@@ -41,7 +44,10 @@ func (controller *Controllers) CreateOrder(context *gin.Context) {
 func (controller *Controllers) GetOrders(context *gin.Context) {
 	database := controller.Database
 	var orders []models.Order
-	if err := database.Preload("Items").Find(&orders).Error; err != nil {
+	if err := database.
+		Model(&models.Order{}).
+		Preload("Items").
+		Find(&orders).Error; err != nil {
 		log.Println("failed to get data from database")
 		context.AbortWithError(http.StatusInternalServerError, err)
 		return
@@ -67,10 +73,24 @@ func (controller *Controllers) UpdateOrder(context *gin.Context) {
 	}
 
 	database := controller.Database
-	if err := database.Model(&models.Order{}).Where("id = ?", orderId).Updates(newOrder).Error; err != nil {
-		log.Println("failed to update the record")
+	if err := database.
+		Model(&models.Order{OrderId: orderId}).
+		Updates(newOrder).Error; err != nil {
+		log.Println("failed to update order of the record")
 		context.AbortWithError(http.StatusInternalServerError, err)
 		return
+	}
+
+	for _, item := range newOrder.Items {
+		if err := database.
+			Model(&models.Item{ItemId: item.ItemId}).
+			Where("order_id = ?", orderId).
+			Omit("OrderId").
+			Updates(item).Error; err != nil {
+			log.Println("failed to update items of into record")
+			context.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
 	}
 
 	log.Println("succeeded to update the record")
@@ -86,7 +106,9 @@ func (controller *Controllers) DeleteOrder(context *gin.Context) {
 	}
 
 	database := controller.Database
-	if err := database.Where("id = ?", orderId).Delete(&models.Order{}).Error; err != nil {
+	if err := database.
+		Select("Items").
+		Delete(&models.Order{OrderId: orderId}).Error; err != nil {
 		log.Println("failed to delete the record")
 		context.AbortWithError(http.StatusInternalServerError, err)
 		return
